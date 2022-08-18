9c28ac1e5e0e0fbda29cebd24f04b3ca829a1222
riahandayani <111479828+riahandayani@users.noreply.github.com>
2022-08-18 14:01:38 +0700

    Add files via upload

---
+++ python2_ria.py
@@ -0,0 +1,8 @@
+nama_depan="Ria"
+nama_tengah="dwi"
+nama_belakang="handayani"
+nama_lengkap="ria dwi safitri handayani"
+print("nama depan saya adalah"nama_depan)
+print("nama tengah saya adalah"nama_tengah)
+print("nama belakang saya adalah"nama_belakang)
+print("nama lengkap saya adalah"nama_lengkap)
