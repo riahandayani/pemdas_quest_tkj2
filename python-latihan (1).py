7ae06e7b2690f079beec99f62e9aad6740c314d6
riahandayani <111479828+riahandayani@users.noreply.github.com>
2022-08-22 13:42:29 +0700

    Add files via upload

---
+++ python-latihan (1).py
@@ -0,0 +1,19 @@
+print("Halo semuanya")
+nama_saya_adalah = input("\nnama saya adalah")
+print("\nHari ini kita belajar fisika")
+print("kita akan belajar pemrograman dasar")
+print("kita mulai pelajaran ini pukul 07.15 \npelajaran ipa")
+
+x = int(input("contoh rumus fisika"))
+y = (kecepatan/waktu)
+print(x, "kecepatan/watktu")
+
+print("\nmarilah pahami rumus fisika ini")
+
+A = int(input("\nTentukan rumus fisika ini: "))
+B = float(input("\nMasukkan nilai bilangan tidak bulat :"))
+c = A + bulat
+
+print("A -", A)
+print("B -", B)
+print("C -", C)
---
+++ python-latihan.py
@@ -0,0 +1,11 @@
+print("Halo semuanya")
+nama_saya_adalah = input("\nnama saya adalah")
+print("\nHari ini kita belajar fisika")
+print("kita akan belajar pemrograman dasar")
+print("kita mulai pelajaran ini pukul 07.15 \npelajaran ipa")
+
+x = int(input("contoh rumus fisika"))
+y = (kecepatan/waktu)
+print(x, "kecepatan/waktu")
+
+print("\nmarilah pahami rumus fisika ini")
